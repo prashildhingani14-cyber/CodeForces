613930b959285a3fed31a842e1c2b576481032c9
prashildhingani14-cyber <prashildhingani14@gmail.com>
2026-07-17 18:42:15 +0530

    Add files via upload

---
+++ ConstructAnArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ConstructAnArray  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            for (int i = 2; i <= n; i += 2)
+                System.out.print(i + " ");
+
+            for (int i = 1; i <= n; i += 2)
+                System.out.print(i + " ");
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
